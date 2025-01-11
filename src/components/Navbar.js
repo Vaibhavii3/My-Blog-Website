67ea457729d545f611bb5741ada888c1cafe8e79
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-01-11 13:44:48 +0530

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,28 +3,24 @@ import "../style/Navbar.css";
 import About from "./About";
 
 const Navbar = () => {
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-    return (
-
-      <div>
-
+  return (
+    <div>
       <button className="hamburger" onClick={toggleMenu}>
         ☰
       </button>
-      <nav className={`sidebar ${isMenuOpen ? 'open' : ''}`}>
+      <nav className={`navbar ${isMenuOpen ? 'open' : ''}`}>
         <ul>
           <About />
         </ul>
       </nav>
-      </div>
-    );
-  };
-  
-  export default Navbar;
-  
+    </div>
+  );
+};
+
+export default Navbar;
